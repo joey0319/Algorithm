5f8caa82ee6d7ac24d6abd0edf10677500d58872
이동주 <joey0319@naver.com>
2022-03-16 20:26:40 +0900

    heap 추가

--- 18.dfs.py
+++ 18.dfs.py
@@ -10,13 +10,13 @@ def dfs(s):
 
     while st:
         s = st.pop()  #pop()은 리스트에서 마지막 원소를 삭제하고 삭제한 원소를 반환해준다.
+        visited[s] = True # dfs는 여기서 방문 기록을 해줘야 한다.
         if s == G:
             return 1 # G(목표점)까지 도착하면 1을 리턴한다.
         else:
             for e in close[s]:  # close[s]에는 s에서 갈 수 있는 노드의 번호가 들어있다.
                 if visited[e] == False:  # e를 방문하지 않았다면
                     st.append(e)    # 스택에 e를 추가하고 / for문을 돌면서 다 추가하는 이유는 그 뒤로 조건에 만족이 안되면 pop을 해서 지워 주고 저장되어 있던 다른 원소를 검사할 것이기 때문
-                    visited[e] == True # visited에도 추가하여 방문했다고 표시한다.
 
     return 0 #스택이 공백이 될 때까지 다 돌았는데도 조건을 만족하는 경로가 없으면 0을 리턴한다.
 
